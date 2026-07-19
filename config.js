2e89e2188972b0324e2865b23ec95ee24628eff6
DON FURY <DONxFURY@proton.me>
2026-07-18 21:16:01 -0800

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const PARENT_DOMAIN = "YOURNAME.github.io";
 
 // Add or remove iframe URLs here.
 const STREAMS = [
-  "https://player.twitch.tv/?channel=kaicenat&parent=www.example.com",
+  "https://player.twitch.tv/?channel=kaicenat&parent=twitch.tv",
   "https://player.twitch.tv/?channel=shroud&parent=YOURNAME.github.io",
   "https://player.twitch.tv/?channel=xqc&parent=YOURNAME.github.io"
 ];
