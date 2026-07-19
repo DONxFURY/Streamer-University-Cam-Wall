d6e4b6ceb00840397ac531d021952df15afff528
DON FURY <DONxFURY@proton.me>
2026-07-18 21:21:50 -0800

    Update iframe URL for kaicenat channel

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const PARENT_DOMAIN = "donxfury.github.io";
 
 // Add or remove iframe URLs here.
 const STREAMS = [
-  "https://player.twitch.tv/?channel=kaicenat&parent=twitch.tv",
+  "https://player.twitch.tv/?channel=kaicenat&parent=donxfury.github.io",
   "https://player.twitch.tv/?channel=shroud&parent=YOURNAME.github.io",
   "https://player.twitch.tv/?channel=xqc&parent=YOURNAME.github.io"
 ];
